32872045639b7e579d61f271e762944621ec4591
nayem181 <nayemimranhasan@gmail.com>
2025-12-24 17:44:00 +0600

    added footer in the mainLayouts

--- src/Layouts/MainLayouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts/MainLayouts.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../../Components/Navbar';
+import Footer from '../../Components/Footer';
 
 const MainLayouts = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Footer></Footer>
         </div>
     );
 };
