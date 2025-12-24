8d8241117c4335aca3da28bb50cd7f4444ff63f1
nayem181 <nayemimranhasan@gmail.com>
2025-12-24 16:18:50 +0600

    make routes and using framer motion

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,8 +1,13 @@
 import { createBrowserRouter } from "react-router";
+import NotFound from "../Pages/Error/NotFound";
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <div>hi</div>
+    },
+    {
+        path: 'not-found',
+        element:<NotFound></NotFound>
     }
 ])
