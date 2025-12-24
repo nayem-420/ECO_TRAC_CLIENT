7349e660cf5d8a0b4c6ffb2d30fa8826cb0c4574
nayem181 <nayemimranhasan@gmail.com>
2025-12-24 18:27:03 +0600

    added banner in the homepage

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
-            <div>hi</div>
+            <Banner></Banner>
         </div>
     );
 };
