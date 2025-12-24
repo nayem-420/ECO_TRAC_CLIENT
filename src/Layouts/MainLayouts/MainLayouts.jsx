cd12269b05552f2e96b04520dfd18f048eef8fe5
nayem181 <nayemimranhasan@gmail.com>
2025-12-24 18:05:20 +0600

    creatin loader and use it

--- src/Layouts/MainLayouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts/MainLayouts.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import Navbar from '../../Components/Navbar';
 import Footer from '../../Components/Footer';
+import { Outlet } from 'react-router';
+import { useRouteLoading } from '../../utils/useRouteLoading';
+import Loading from '../../Components/Loading';
 
 const MainLayouts = () => {
+
+    const loading = useRouteLoading(2000);
     return (
-        <div>
-            <Navbar></Navbar>
-            <Footer></Footer>
-        </div>
+      <div>
+        <Navbar></Navbar>
+        {loading && <Loading></Loading>}
+        <main className="flex flex-col min-h-screen px-4 py-8 container mx-auto">
+          <Outlet></Outlet>
+        </main>
+        <Footer></Footer>
+      </div>
     );
 };
 
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,14 +1,21 @@
 import { createBrowserRouter } from "react-router";
 import NotFound from "../Pages/Error/NotFound";
 import MainLayouts from "../Layouts/MainLayouts/MainLayouts";
+import Home from "../Pages/Home/Home";
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <MainLayouts></MainLayouts>
-    },
-    {
-        path: '*',
-        element:<NotFound></NotFound>
-    }
-])
+  {
+    path: "/",
+    element: <MainLayouts></MainLayouts>,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound></NotFound>,
+  },
+]);
