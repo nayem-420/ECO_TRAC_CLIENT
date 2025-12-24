7680ecf4cdf0a653aeb2495b8fa171722cb4944f
nayem181 <nayemimranhasan@gmail.com>
2025-12-24 20:43:45 +0600

    added register page

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router";
 import NotFound from "../Pages/Error/NotFound";
 import MainLayouts from "../Layouts/MainLayouts/MainLayouts";
 import Home from "../Pages/Home/Home";
+import AuthLayout from "../Layouts/AuthLayouts/AuthLayout";
+import Registers from "../Pages/Auth/Registers";
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +13,16 @@ export const router = createBrowserRouter([
       {
         index: true,
         element: <Home></Home>,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <AuthLayout></AuthLayout>,
+    children: [
+      {
+        path: "register",
+        element: <Registers></Registers>,
       },
     ],
   },
